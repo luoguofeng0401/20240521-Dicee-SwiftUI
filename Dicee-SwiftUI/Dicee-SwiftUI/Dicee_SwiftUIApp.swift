65203032e8a4da4408cc1444da869d25dcce7320
luoguofeng0401 <56261249+luoguofeng0401@users.noreply.github.com>
2024-05-21 15:58:10 +0800

    Add files via upload

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Guo-Feng Luo on 2024/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
